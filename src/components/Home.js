2aaadb38ae8c6b60ee86e4ab4261d602037a6107
Michael Wong <michaelwong1391@gmail.com>
2021-09-18 14:15:24 -0400

    second page setup, just needs, image additions, a description of our app, and links/redirects

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,24 @@ export default function Home() {
    return (
       <>
          <div id="navbar">
-            <p>Log Out</p>
+            <p id="logout">Log Out</p>
          </div>
+         <div class="boxes" id="box1">
+            <p>Drip Me Out</p>
+            <p class="smalltext">Have an outfit created for you based on the weather and your preferences</p>
+         </div>
+         <div class="boxes" id="box2">
+            <p>Customize an Outfit</p>
+            <p class="smalltext">Create your own outfit based on items you have in your closet</p>
+         </div>
+         <div class="boxes" id="box3">
+            <p>My Closet</p>
+            <p class="smalltext">Browse and add items to your very own virtual closet</p>
+         </div>
+         <div id="dripmeout">
+            <p>Drip Me Out is lorem ipsum dolor sit amet.</p>
+         </div>
+
       </>
    )
 }
