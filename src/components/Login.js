30bed4bc7d32151a3326444433a672166c5b1e7e
Rachel Tieu <rachel.tieu02@myhunter.cuny.edu>
2021-09-19 15:23:35 -0400

    created and styled home page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,38 @@
 import '../css/login.css';
+import Button from './Button.js';
 
 export default function Login() {
    return (
       <>
-         <div id="title">
-            <h1>Drip Me Out</h1>
-         </div>
-         <div id="login">
-            <p>E-mail</p>
-            <p>Password</p>
-         </div>
+        <div id="login-container">
+           <div id="title">
+              <h1>Drip Me Out</h1>
+           </div>
+
+           <br/>
+
+           <div id="login">
+              <form id="login-form">
+                <div class="login-fields">
+                  <label id="email-field" for="email-login"> Email: </label>
+                  <br/>
+                  <input type="email" id="email-login"/>
+                </div>
+
+                <div class="login-fields">
+                  <label id="password-field" for="password-login"> Password: </label>
+                  <br/>
+                  <input type="password" id="password-login"/>
+                </div>
+
+                <div id="login-buttons">
+                  <Button text = "Login" />
+                  <Button text = "Sign Up" /> 
+                </div>
+
+              </form>
+           </div>
+        </div>
       </>
    )
 }
