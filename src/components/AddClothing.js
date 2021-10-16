18a60f96bf3764d31920d99d129b0b55d63f18b4
bettyashley <bettytuckerr@gmail.com>
2021-10-16 03:55:58 -0400

    Update add clothing page.

--- src/components/AddClothing.js
+++ src/components/AddClothing.js
@@ -1,7 +1,6 @@
 import '../css/addclothing.css';
+import Button from './Button.js';
 // import Header from './Header'
-import ClothingForm from './ClothingForm'
-import ClothingImage from './ClothingImage'
 
 const AddClothing = () => {
 
@@ -11,8 +10,42 @@ const AddClothing = () => {
         <a href="/" id="logout">Log Out</a>
       </div>
       <div className='content-container'>
-        <ClothingForm />
-        <ClothingImage />
+        <div id="clothingform">
+           <div id="innerdiv">
+             <form id="clothing-form">
+
+               <div class="clothing-fields">
+                 <label id="label-field" for="clothing-label"> Clothing Label: </label>
+                 <br/>
+                 <input type="input" id="clothing-label" required/>
+               </div>
+
+               <div class="clothing-fields">
+                 <label id="category-field" for="clothing-category"> Category: </label>
+                 <br/>
+                 <input type="input" id="clothing-category" required/>
+               </div>
+
+               <div class="clothing-fields">
+                 <label id="type-field" for="clothing-type"> Type: </label>
+                 <br/>
+                 <input type="input" id="clothing-type" required/>
+               </div>
+
+               <div class="clothing-fields">
+                 <label id="color-field" for="clothing-color"> Color: </label>
+                 <br/>
+                 <input type="input" id="clothing-color" required/>
+               </div>
+
+             </form>
+           </div>
+        </div>
+        <div id="clothingimage">
+           <div id="innerdiv">
+              Clothing Image
+           </div>
+        </div>
       </div>
     </div>
   )
