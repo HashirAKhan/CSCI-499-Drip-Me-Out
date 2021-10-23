16185acc0c6967c42c5968ea22b60c5819b2eea6
Michael Wong <michaelwong1391@gmail.com>
2021-10-23 19:06:49 -0400

    clothing items can be added to personal database now, no duplicate images allowed

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,11 +12,9 @@ const Button = ({ text, href, image, save }) => {
     if (image) {
 
       let file = document.getElementById("myFile").files[0];
-      if (file)
-      {
+      if (file) {
         image_displayed = false;
-        if (document.getElementById("clothing_img"))
-        {
+        if (document.getElementById("clothing_img")) {
           document.getElementById("clothing_img").remove();
         }
       }
@@ -35,7 +33,54 @@ const Button = ({ text, href, image, save }) => {
       }
     }
     if (save) {
+      let label = document.getElementById("clothing-label").value;
+      let color = document.getElementById("clothing-color").value;
+      if (document.getElementById("category").value === "select" || document.getElementById("type").value === "select") {
+        alert("Category and Type required");
+      }
+      else if (!document.getElementById("clothing_img")) {
+        alert("Image upload required (must press upload)")
+      }
+      else if (label === "" || color === "") {
+        alert("Label and color required")
+      }
+      else {
+        let file = document.getElementById("myFile").files[0]
+        const reader = new FileReader();
 
+        reader.addEventListener("load", function () {
+          // convert image file to base64 string
+          let base64 = reader.result;
+          send_data_to_db(base64);
+        }, false);
+
+        if (file) {
+          reader.readAsDataURL(file);
+        }
+        function send_data_to_db(base64) {
+          let img = document.getElementById("clothing_img").src;
+          let xhr = new XMLHttpRequest();
+          xhr.addEventListener("load", () => {
+            if (xhr.responseText === "recorded")
+            {
+              alert("Add unsuccessful. Item has already been added to database");
+            }
+            else if (xhr.responseText === "added")
+            {
+              alert("Item added to database");
+            }
+          });
+
+          xhr.open("POST", "http://localhost:8080/additem");
+          let category = document.getElementById("category").value;
+          let type = document.getElementById("type").value;
+          let email = localStorage.getItem('email');
+          let password = localStorage.getItem('password');
+          let adding_item = `${type}&${category}&${label}&${color}&${base64}&${email}&${password}`
+          xhr.send(adding_item);
+        }
+
+      }
     }
   }
 
