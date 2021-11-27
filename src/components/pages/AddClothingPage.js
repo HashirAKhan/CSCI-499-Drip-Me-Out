1cd4acf5c31978b6696fb4b8b499991d703e9ff5
Michael Wong <michaelwong1391@gmail.com>
2021-11-27 14:28:07 -0500

    fixed subcategory issue

--- src/components/pages/AddClothingPage.js
+++ src/components/pages/AddClothingPage.js
@@ -5,85 +5,212 @@ import { useEffect } from "react";
 // import Header from './Header'
 
 const AddClothing = () => {
-
   useEffect(() => {
-
     document.getElementById("category").onchange = function category() {
-        let category = document.getElementById("category").value;
-        let subcategory = document.getElementById("subcategory");
+      let category = document.getElementById("category").value;
+      let subcategory = document.getElementById("subcategory");
+      while (subcategory.firstChild) {
+        subcategory.firstChild.remove();
+      }
 
-        if (category === "Coats & Jackets") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Coat", "coat");
-          document.getElementById("subcategory").options[2] = new Option("Jacket", "jacket");
-          document.getElementById("subcategory").options[3] = new Option("Parka", "parka");
-          document.getElementById("subcategory").options[4] = new Option("Raincoat", "raincoat");
-        }
-        else if (category === "Dresses") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Minidress", "minidress");
-          document.getElementById("subcategory").options[2] = new Option("Mididress", "mididress");
-          document.getElementById("subcategory").options[3] = new Option("Maxidress", "maxidress");
-        }
-        else if (category === "Hoodies & Sweatshirts") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Hoodie", "hoodie");
-          document.getElementById("subcategory").options[2] = new Option("Sweatshirt", "sweatshirt");
-        }
-        else if (category === "Jumpsuits & Rompers") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Jumpsuit", "jumpsuit");
-          document.getElementById("subcategory").options[2] = new Option("Romper", "romper");
-        }
-        else if (category === "Pants") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Cargo pants", "cargo pants");
-          document.getElementById("subcategory").options[2] = new Option("Jeans", "jeans");
-          document.getElementById("subcategory").options[3] = new Option("Pants", "pants");
-          document.getElementById("subcategory").options[4] = new Option("Sweatpants", "sweatpants");
-        }
-        else if (category === "Shoes") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Boots", "boots");
-          document.getElementById("subcategory").options[2] = new Option("Dress shoes", "dress shoes");
-          document.getElementById("subcategory").options[3] = new Option("Flat shoes", "flat shoes");
-          document.getElementById("subcategory").options[4] = new Option("Heels", "heels");
-          document.getElementById("subcategory").options[5] = new Option("Sandals", "sandals");
-          document.getElementById("subcategory").options[6] = new Option("Slides & Flip flops", "slides & flip flops");
-          document.getElementById("subcategory").options[7] = new Option("Sneakers", "sneakers");
-        }
-        else if (category === "Shorts") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Shorts", "shorts");
-        }
-        else if (category === "Skirts") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Miniskirt", "miniskirt");
-          document.getElementById("subcategory").options[2] = new Option("Midiskirt", "midiskirt");
-          document.getElementById("subcategory").options[3] = new Option("Maxiskirt", "maxiskirt");
-        }
-        else if (category === "Sweaters & Cardigans") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Cardigan", "cardigan");
-          document.getElementById("subcategory").options[2] = new Option("Sweater", "sweater");
-        }
-        else if (category === "Tops") {
-          document.getElementById("subcategory").options[0] = new Option("Select sub-category", "");
-          document.getElementById("subcategory").options[1] = new Option("Tank top", "tank top");
-          document.getElementById("subcategory").options[2] = new Option("Crop top", "crop top");
-          document.getElementById("subcategory").options[3] = new Option("Blouse", "blouse");
-          document.getElementById("subcategory").options[4] = new Option("Bodysuit", "bodysuit");
-          document.getElementById("subcategory").options[5] = new Option("Button-up shirt", "button-up shirt");
-          document.getElementById("subcategory").options[6] = new Option("Long-sleeve t-shirt", "long-sleeve");
-          document.getElementById("subcategory").options[7] = new Option("Short-sleeve t-shirt", "short-sleeve");
-        }
-      };
+      if (category === "Coats & Jackets") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Coat",
+          "coat"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Jacket",
+          "jacket"
+        );
+        document.getElementById("subcategory").options[3] = new Option(
+          "Parka",
+          "parka"
+        );
+        document.getElementById("subcategory").options[4] = new Option(
+          "Raincoat",
+          "raincoat"
+        );
+      } else if (category === "Dresses") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Minidress",
+          "minidress"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Mididress",
+          "mididress"
+        );
+        document.getElementById("subcategory").options[3] = new Option(
+          "Maxidress",
+          "maxidress"
+        );
+      } else if (category === "Hoodies & Sweatshirts") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Hoodie",
+          "hoodie"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Sweatshirt",
+          "sweatshirt"
+        );
+      } else if (category === "Jumpsuits & Rompers") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Jumpsuit",
+          "jumpsuit"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Romper",
+          "romper"
+        );
+      } else if (category === "Pants") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Cargo pants",
+          "cargo pants"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Jeans",
+          "jeans"
+        );
+        document.getElementById("subcategory").options[3] = new Option(
+          "Pants",
+          "pants"
+        );
+        document.getElementById("subcategory").options[4] = new Option(
+          "Sweatpants",
+          "sweatpants"
+        );
+      } else if (category === "Shoes") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Boots",
+          "boots"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Dress shoes",
+          "dress shoes"
+        );
+        document.getElementById("subcategory").options[3] = new Option(
+          "Flat shoes",
+          "flat shoes"
+        );
+        document.getElementById("subcategory").options[4] = new Option(
+          "Heels",
+          "heels"
+        );
+        document.getElementById("subcategory").options[5] = new Option(
+          "Sandals",
+          "sandals"
+        );
+        document.getElementById("subcategory").options[6] = new Option(
+          "Slides & Flip flops",
+          "slides & flip flops"
+        );
+        document.getElementById("subcategory").options[7] = new Option(
+          "Sneakers",
+          "sneakers"
+        );
+      } else if (category === "Shorts") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Shorts",
+          "shorts"
+        );
+      } else if (category === "Skirts") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Miniskirt",
+          "miniskirt"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Midiskirt",
+          "midiskirt"
+        );
+        document.getElementById("subcategory").options[3] = new Option(
+          "Maxiskirt",
+          "maxiskirt"
+        );
+      } else if (category === "Sweaters & Cardigans") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Cardigan",
+          "cardigan"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Sweater",
+          "sweater"
+        );
+      } else if (category === "Tops") {
+        document.getElementById("subcategory").options[0] = new Option(
+          "Select sub-category",
+          ""
+        );
+        document.getElementById("subcategory").options[1] = new Option(
+          "Tank top",
+          "tank top"
+        );
+        document.getElementById("subcategory").options[2] = new Option(
+          "Crop top",
+          "crop top"
+        );
+        document.getElementById("subcategory").options[3] = new Option(
+          "Blouse",
+          "blouse"
+        );
+        document.getElementById("subcategory").options[4] = new Option(
+          "Bodysuit",
+          "bodysuit"
+        );
+        document.getElementById("subcategory").options[5] = new Option(
+          "Button-up shirt",
+          "button-up shirt"
+        );
+        document.getElementById("subcategory").options[6] = new Option(
+          "Long-sleeve t-shirt",
+          "long-sleeve"
+        );
+        document.getElementById("subcategory").options[7] = new Option(
+          "Short-sleeve t-shirt",
+          "short-sleeve"
+        );
+      }
+    };
 
-      document.body.style.background = "url(https://i.ibb.co/h8RZwhY/base.jpg)"
-      document.body.style.backgroundSize = "cover"
-      document.body.style.backgroundAttachment = "fixed"
-      document.body.style.backgroundPosition = "center"
-    }, []);
+    document.body.style.background = "url(https://i.ibb.co/h8RZwhY/base.jpg)";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundPosition = "center";
+  }, []);
 
   return (
     <div className="container">
@@ -112,19 +239,28 @@ const AddClothing = () => {
                   <option value="select">Select</option>
                   <option value="Coats & Jackets">Coats & Jackets</option>
                   <option value="Dresses">Dresses</option>
-                  <option value="Hoodies & Sweatshirts">Hoodies & Sweatshirts</option>
-                  <option value="Jumpsuits & Rompers">Jumpsuits & Rompers</option>
+                  <option value="Hoodies & Sweatshirts">
+                    Hoodies & Sweatshirts
+                  </option>
+                  <option value="Jumpsuits & Rompers">
+                    Jumpsuits & Rompers
+                  </option>
                   <option value="Pants">Pants</option>
                   <option value="Shoes">Shoes</option>
                   <option value="Shorts">Shorts</option>
                   <option value="Skirts">Skirts</option>
-                  <option value="Sweaters & Cardigans">Sweaters & Cardigans</option>
+                  <option value="Sweaters & Cardigans">
+                    Sweaters & Cardigans
+                  </option>
                   <option value="Tops">Tops</option>
                 </select>
               </div>
 
               <div class="clothing-fields">
-                <label id="subcategory-field" for="clothing-subcategory"> Sub-Category: </label>
+                <label id="subcategory-field" for="clothing-subcategory">
+                  {" "}
+                  Sub-Category:{" "}
+                </label>
                 <br />
                 {/* <input type="input" id="clothing-subcategory" required /> */}
                 <select onchange="" id="subcategory">
@@ -162,7 +298,10 @@ const AddClothing = () => {
               </div>
 
               <div class="clothing-fields">
-                <label id="color2-field" for="clothing-color2"> Secondary Color: </label>
+                <label id="color2-field" for="clothing-color2">
+                  {" "}
+                  Secondary Color:{" "}
+                </label>
                 <br />
                 {/* <input type="input" id="clothing-color" required /> */}
                 <select id="color2">
